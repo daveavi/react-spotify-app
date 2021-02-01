7c6ecc1a976fe2b0b198c0dd8f1780c22f22083b
daveavi <avidave97@gmail.com>
2021-02-01 14:58:08 -0500

    able to get current song I am playing in spotify

--- app/src/App.js
+++ app/src/App.js
@@ -11,16 +11,20 @@ class App extends Component{
     const params = this.getHashParams()
     this.state = {
        nowPlaying: {
-         name: "Not Checked",
-         image: ''
+         gotName : false, 
+         name: "",
+         image: '',
        },
        loggiedIn : params.access_token ? true : false 
     }
 
     if (params.access_token){
       console.log(params.access_token)
-      spotifyWebApi.setAccessToken("BQB1RqmdtB-pglm6ZIou6KM_l50FnKSdFaBxDpHtkbhtP8eCTWRujL-BBwrMnLLAa6dxgQhWxsP1BTLus4vbTy0ffXXRQeTf7tYJHR_iRemixLpbBHv3a9oeaVl66tta-aW-f1ilv_EeCg4nkysbcrhxehdVQHOtoMZ2MpxWw1dMJckxm7xnDDS_xvs")
+      spotifyWebApi.setAccessToken(params.access_token)
     }
+
+    this.getNowPlaying = this.getNowPlaying.bind(this)
+
 
   }
 
@@ -38,12 +42,12 @@ class App extends Component{
   getNowPlaying(){
     spotifyWebApi.getMyCurrentPlayingTrack().then((response) => {
           console.log(response)
-          // this.setState({
-          //   nowPlaying:{
-          //     name: response.item.name,
-          //     image : response.item.album.images[0].url
-          //   }
-          // })
+          this.setState({
+            nowPlaying:{
+              name: response.item.name,
+              image : response.item.album.images[0].url
+            }
+          })
       })
   }
 
@@ -56,12 +60,14 @@ class App extends Component{
         </h1>
         <div className ="ButtonContainer">
           {this.state.loggiedIn 
-            ? null
+            ? <button onClick={this.getNowPlaying} className="ButtonSize">What song is playing right now?</button>
             : <LoginButton/>
           }
-          <button onClick={this.getNowPlaying} className="ButtonSize">What song is playing right now?</button>
         </div>
-       
+          {this.state.nowPlaying.name
+            ? <CurrentSong name={this.state.nowPlaying.name} image = {this.state.nowPlaying.image}/>
+            : null
+          } 
 
 
 
@@ -73,18 +79,18 @@ class App extends Component{
   }
 }
 
-// class CurrentSong extends Component {
-//   render(){
-//     return(
-//       <div>
-//           <h1>Now Playing: {this.props.name}</h1>
-//           <div>
-//             <img src = {this.props.image} style={{width: 100}}/>
-//           </div>
-//       </div>
-//     );
-//   }
-// }
+class CurrentSong extends Component {
+  render(){
+    return(
+      <div>
+          <h1>Now Playing: {this.props.name}</h1>
+          <div>
+            <img src = {this.props.image} style={{width: 100}}/>
+          </div>
+      </div>
+    );
+  }
+}
 
 class LoginButton extends Component{
   render(){
--- auth-server/authorization_code/app.js
+++ auth-server/authorization_code/app.js
@@ -14,6 +14,9 @@ var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
  // Your redirect uri
+ var client_id = "04dec92dc5584867b6769643eb7b479d"
+ var client_secret = "76a688013d0d400fb2ca842fdd3dfa89"
+ var redirect_uri = "http://localhost:8888/callback"
 
 /**
  * Generates a random string containing numbers and letters
